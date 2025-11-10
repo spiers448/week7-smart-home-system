3bfd86e6bed9b92046a6cbb3fe131dc29a589554
spiers448 <spiers-b3@ulster.ac.uk>
2025-11-10 16:00:42 +0000

    updated smart device class

--- SmartHome/SmartDevice.cs
+++ SmartHome/SmartDevice.cs
@@ -8,5 +8,50 @@ namespace SmartHome
 {
     internal class SmartDevice
     {
+        //private fields
+        private int deviceId;
+        private string deviceName;
+        private bool status;
+
+        //public fields
+        public int DeviceID
+        {
+            get { return deviceId; }
+            set { deviceId = value; }
+        }
+        public string DeviceName
+        {
+            get { return deviceName; }
+            set { deviceName = value; }
+        }
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        //constructor
+        public SmartDevice(int deviceId, string deviceName, bool status)
+        {
+            this.deviceId = deviceId;
+            this.deviceName = deviceName;
+            this.status = status;
+        }
+
+        //turn on method
+        public void turnOn()
+        {
+            status = true;
+        }
+        //turn off method
+        public void turnOff()
+        {
+            status = false;
+        }
+        //get status method
+        public bool getStatus()
+        {
+            return status;
+        }
     }
 }
